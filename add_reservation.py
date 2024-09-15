40c1e6e7f472ce8ea1413c135b5e9d4bb2ae0684
Sukruthi <sukruthisantosh@gmail.com>
2024-09-15 21:01:42 +0100

    bookings from json added to table

---
+++ add_reservation.py
@@ -0,0 +1,24 @@
+import json
+from decimal import Decimal
+import boto3
+
+def load_booking(booking_list, dynamodb=None):
+
+    if dynamodb is None:
+        dynamodb = boto3.resource('dynamodb')
+
+    booking_table = dynamodb.Table('Reservations')
+
+    for booking in booking_list:
+        booking_id = booking['booking_id']
+        name = booking['full_name']
+
+        print("Displaying booking data:", booking_id, name)
+
+        booking_table.put_item(Item=booking)
+
+if __name__ == '__main__':
+    with open("booking_summary.json") as json_file:
+        booking_list = json.load(json_file, parse_float=Decimal)
+
+    load_booking(booking_list)
